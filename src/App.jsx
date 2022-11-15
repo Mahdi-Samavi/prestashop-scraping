ab5e036a4ab707164036fa5bd7fd1d4ad6f073a9
Mahdi Samavi <samavi.mahdi@gmail.com>
2022-11-16 00:11:52 +0330

    Header created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header, Sidebar } from "./Components";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div>
+        <Sidebar />
+      </div>
+    </>
+  );
+}
